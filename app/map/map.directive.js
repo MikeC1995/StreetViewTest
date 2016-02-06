9184227a9aabaa26ba4da23329e7c9904ed89ac9
MikeC1995 <mike_christensen@btinternet.com>
2016-02-06 18:00:35 +0000

    mesh is filtered to only contain points which have SV data associated

--- app/map/map.directive.js
+++ app/map/map.directive.js
@@ -8,12 +8,13 @@ app.directive('map', ['loadGoogleMapAPI', function(loadGoogleMapAPI) {
         mapId: '@id'
       },
       controller: ['$scope', function($scope) {
-        $scope.density = 20;
+        $scope.density = 20;  // density of street view point mesh
+        $scope.coords = [];   // list of street view-able lat-lng coords
 
-        // Function for computing a mesh of lat long coordinates
-        // within the bbox given by the south-west and north-easterly points,
-        // with a given density.
-        $scope.computeLatLngMesh = function(sw, ne, density) {
+        /* Computes a mesh of lat-lng coords in the bbox spanning
+        ** sw to ne with specified density.
+        */
+        function computeLatLngMesh(sw, ne, density) {
           var lat_step = (ne.lat - sw.lat)/density;
           var lng_step = (ne.lng - sw.lng)/density;
           var coords = [];
@@ -28,6 +29,32 @@ app.directive('map', ['loadGoogleMapAPI', function(loadGoogleMapAPI) {
           return coords;
         }
 
+        /* Callback for sv.getPanorama
+        ** If the requested coordinate has Street View data, this
+        ** coordinate is added to the list and a marker added to the map.
+        */
+        function processSVData(data, status) {
+          if (status === google.maps.StreetViewStatus.OK) {
+            var latlng = {
+              lat: data.location.latLng.lat(),
+              lng: data.location.latLng.lng(),
+            }
+            $scope.coords.push(latlng);
+            addMarker(latlng);
+          }
+        }
+
+        /* Calls sv.getPanorama for each coordinate in a mesh.
+        ** The coordinates which have Street View data will be added to
+        ** $scope.coords and a marker added to the map.
+        */
+        function filterStreetView(coords) {
+          for(var i = 0; i < coords.length; i++) {
+            $scope.sv.getPanorama({location: coords[i], radius: 50}, processSVData);
+          }
+        }
+
+        // Adds a marker to the map.
         function addMarker(latlng) {
           new google.maps.Marker({
             position: latlng,
@@ -35,35 +62,36 @@ app.directive('map', ['loadGoogleMapAPI', function(loadGoogleMapAPI) {
           });
         }
 
+        // Computes a mesh of lat-lng coords and filters them for SV data.
+        function addMesh(latlng1, latlng2) {
+          var mesh = computeLatLngMesh(latlng1, latlng2, $scope.density);
+          filterStreetView(mesh);
+        }
+
+        /* Called when user click on the map.
+        ** First click sets the sw corner of the bbox.
+        ** Second click sets the ne corner of the bbox, and builds the mesh.
+        */
         $scope.addMeshCorner = function(latlng) {
           if($scope.sw === undefined) {
             $scope.sw = latlng;
             addMarker($scope.sw);
           } else if($scope.ne === undefined) {
             $scope.ne = latlng;
-            $scope.addMesh($scope.sw, $scope.ne)
+            addMesh($scope.sw, $scope.ne)
           }
         }
-
-        $scope.addMesh = function(latlng1, latlng2) {
-          var coords = $scope.computeLatLngMesh(latlng1, latlng2, $scope.density);
-          for(var i = 0; i < coords.length; i++) {
-            addMarker(coords[i]);
-          }
-        }
-
       }],
       link: function($scope, elem, attrs) {
         // Loads google map script
         loadGoogleMapAPI.then(function () {
-            // Promised resolved
             $scope.initialize();
         }, function () {
-            // Promise rejected
             console.error("Couldn't load Google Maps API!");
         });
 
         $scope.initialize = function() {
+          $scope.sv = new google.maps.StreetViewService();
           $scope.map = new google.maps.Map(document.getElementById($scope.mapId), {
             zoom: 10,
             center: {lat: 51.552015, lng: -2.452623}
